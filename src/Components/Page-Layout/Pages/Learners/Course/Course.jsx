005a3b2e94234273f9e8201e3b1a8f51cb458cd9
Chamith Hirushan <chamith.ictech@gmail.com>
2023-02-19 06:19:40 +0530

    today

--- src/Components/Page-Layout/Pages/Learners/Course/Course.jsx
+++ src/Components/Page-Layout/Pages/Learners/Course/Course.jsx
@@ -5,6 +5,7 @@ import CourseData from "../../../../Static-Database/CourseData/CourseData";
 import InputField from "../../../../Re-Used/InputField/InputField";
 import RadioInput from "../../../../Re-Used/RadioInput/RadioInput";
 import SubmitButton from "../../../../Re-Used/SubmitButton/SubmitButton";
+import HoriontalScroller from "../../../../Re-Used/HoriontalScroller/HoriontalScroller";
 
 function Course() {
 
@@ -15,6 +16,8 @@ function Course() {
     const _courseELEMENT = searchParams.get('course')-1;
 
     const dataSet = CourseData[_categoryELEMENT].courses[_courseELEMENT]
+    const courseContents = dataSet.contents
+
     const courseName = dataSet.title + '-' + ' (' + dataSet.level + ')';
 
     const[formData, setFormData] = useState ({
@@ -78,26 +81,29 @@ function Course() {
     ]
 
     return (
-        <div className="Course px-5 py-5">
-            <div className="Course-Title">
-                <h2>{dataSet.title} - {dataSet.level}</h2>
-            </div>
+        <div className="Course bg-white">
+            <div className="bg-light pt-4 pt-md-5">
+                <div className="Course-Title px-3 px-sm-4 px-lg-5">
+                    <h2>{dataSet.title} - <span>{dataSet.level}</span></h2>
+                </div>
 
-            <div className="Course-Description">
-                <p>{dataSet.description}</p>
+                <div className="Course-Description px-3 px-sm-4 px-lg-5">
+                    <p className="mb-md-4 pb-2 pb-md-3">{dataSet.description}</p>
+                </div>
             </div>
+            
 
-            <div className="row g-0">
-                <div className="col-7">
-                    <div className="Course-Information">
-                        <div className="Join-Course">
-                            <h6>WHO CAN JOIN ?</h6>
+            <div className="row g-0 pt-4 pt-sm-5 px-lg-5 bg-white">
+                <div className="col-md-7">
+                    <div className="Course-Information pe-md-4">
+                        <div className="Join-Course ps-md-4 ps-lg-0">
+                            <h5>WHO CAN <span>JOIN</span> ?</h5>
                             <p>{dataSet.join}</p>
                         </div>
 
-                        <div className="Course-Details">
-                            <div className="row g-0">
-                                <div className="col-6">
+                        <div className="Course-Details px-3 px-sm-4 px-lg-0">
+                            <div className="row g-0 py-3 py-md-0">
+                                <div className="col-sm-6 d-flex justify-content-start justify-content-sm-center justify-content-md-start">
                                     <div className="Detail-Set">
                                         <h6>Duration : <span>{dataSet.duration}</span></h6>
                                         <h6>Method : <span>{dataSet.method}</span></h6>
@@ -105,10 +111,10 @@ function Course() {
                                     </div>
                                 </div>
 
-                                <div className="col-6">
+                                <div className="col-sm-6 d-flex justify-content-start justify-content-sm-start">
                                     <div className="Detail-Set">
                                         <h6>Attendance : <span>{dataSet.attendance}</span></h6>
-                                        <h6>Course Fee : <span>Rs.{dataSet.fee}</span></h6>
+                                        <h6>Course Fee : <span className="Course-Fee">Rs.{dataSet.fee}</span></h6>
                                     </div>
                                 </div>
                             </div>
@@ -116,8 +122,8 @@ function Course() {
                     </div>
                 </div>
 
-                <div className="col-5">
-                    <div className="Apply-Form">
+                <div className="col-md-5 px-3 px-sm-4 px-lg-0">
+                    <div className="Apply-Form px-3 pt-3 pb-4 mx-0 mx-sm-5 mx-md-0 mx-xl-2">
                         <form action="">
                             <div className="Apply-Course-Field">
 
@@ -134,7 +140,7 @@ function Course() {
                                     </div>
                                  ))}
 
-                                 <div className="Payment-Type">
+                                 <div className="Payment-Type py-2">
                                     <div className="row g-0">
                                         {payments.map((pay) => (
                                             <div className="col-6">
@@ -158,7 +164,50 @@ function Course() {
                 </div>
             </div>
 
-            <button onClick={()=> console.log(formData)}>ffff</button>
+            <div className="Payments bg-light px-3 px-sm-5 px-md-4 px-lg-5 pt-4 pb-2 my-5">
+                <div className="row g-0">
+                    <div className="col-12 col-md-5 col-lg-4 d-flex align-items-center">
+                        <div className="Bank-Details mt-1 mb-3 my-sm-4 my-md-0">
+                            <h6>Account Name : <span>Can Academy (Pvt)Ltd.</span></h6>
+                            <h6>Account Number : <span className="Account-Number">008013390575001</span></h6>
+                            <h6>Bank Name : <span>Seylan Bank </span></h6>
+                            <h6>Branch : <span>Kollupitiya (0080)</span></h6>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-7 col-lg-8 d-flex justify-content-md-center">
+                        <div className="Payment-Instructions ps-md-3">
+                            <h5 className="mb-3">To facilitate identification of your remittance, please quote your name as reference.</h5>
+                            
+                            <div className="Advice">
+                                <div className="Advice-Title">
+                                    <h6>Enroll</h6>
+                                </div>
+
+                                <div className="Advice-Details">
+                                    <p>Pay and send us the receipt via WhatsApp to <span>0742833337</span></p>
+                                </div>
+                            </div>
+
+                            <div className="Advice">
+                                <div className="Advice-Title">
+                                    <h6>Need advice ?</h6>
+                                </div>
+
+                                <div className="Advice-Details">
+                                    <p>Contact our mentors for help <span>(Call hours weekdays 11 am – 6 pm)</span>, More Info: <span>0742833337</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="Selected-Course-Contents px-3 px-sm-4 px-lg-5">
+                {courseContents.map((content) => (
+                    <HoriontalScroller Title={content.contentTitle} List={content.lessons}></HoriontalScroller>
+                ))}
+            </div>
         </div>
     )
     
---
+++ src/Components/Re-Used/HoriontalScroller/HoriontalScroller.jsx
@@ -0,0 +1,31 @@
+import "./HoriontalScroller.css"
+
+const HoriontalScroller =( props )=> {
+
+    const chunkSize = 10;
+    const chunks = [];
+
+    for ( let i = 0; i < props.List.length; i += chunkSize ) {
+        chunks.push(props.List.slice(i, i + chunkSize));
+    }
+
+    return (
+        <div className="HoriontalScroller">
+            <h6>{props.Title}</h6>
+
+            <div className="Side-Scroller bg-light ps-3 pt-3 mb-5">
+                {chunks.map((_chunk) => (
+                    <div className="col-12 col-md-4">
+                        <ul className="List-Item-Line">
+                            {_chunk.map((_line)=> (
+                                <li>{_line}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default HoriontalScroller
